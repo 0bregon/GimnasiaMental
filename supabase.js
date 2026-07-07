fff0014c22577057cce0e1badeb55d6b4135966b
Dino <marielobregon5@gmail.com>
2026-07-07 17:58:53 -0500

    Agregar comentario aclaratorio en la creación de conexión a Supabase

--- supabase.js
+++ supabase.js
@@ -6,7 +6,7 @@
 const SUPABASE_URL = "https://wrfflsfwazrkywknhnak.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyZmZsc2Z3YXpya3l3a25obmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MzgzNDMsImV4cCI6MjA5OTAxNDM0M30.4EjK77x_t8Hhd7Y8ENRtsjIB7p4JdKYIq1eHxtDkv-I";
 
-// Crear conexión
+// Crear conexión (nombre distinto al de la librería para evitar choque)
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
